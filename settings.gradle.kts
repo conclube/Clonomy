f2bf9d62f8ee6b968ea87031d82cc8dc4122573a
Conclure <63319713+Conclure@users.noreply.github.com>
2022-01-31 01:36:30 +0100

    Add api, bukkit, shared and annotation module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,25 @@
 rootProject.name = "Clonomy"
 
+include(
+    ":module-api",
+    ":module-bukkit",
+    ":module-shared",
+    ":module-annotations"
+)
+
+rename(
+    ":module-api" to "api",
+    ":module-bukkit" to "bukkit",
+    ":module-shared" to "shared",
+    ":module-annotations" to "annotations"
+)
+
+fun rename(path: String, newName: String) {
+    findProject(path)?.name = newName
+}
+
+fun rename(vararg pairs: Pair<String,String>) {
+    pairs.forEach {
+        rename(it.first,it.second)
+    }
+}
