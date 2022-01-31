e1f9dc4cb1ce54b7b3d25d90c62baccaf6e41ab3
Conclure <63319713+Conclure@users.noreply.github.com>
2022-01-31 01:47:09 +0100

    Add test logger, shadow jar and blossom gradle plugins and support for modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,61 @@
-plugins {
-    `java-library`
-}
+import com.adarshr.gradle.testlogger.TestLoggerExtension
+import com.adarshr.gradle.testlogger.theme.ThemeType
 
+plugins {
+    id("java-library")
+    id("com.adarshr.test-logger") version "3.1.0" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    id("net.kyori.blossom") version "1.3.0" apply false
+}
 group = "me.conclure"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    mavenCentral()
-}
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "com.adarshr.test-logger")
+    apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "net.kyori.blossom")
 
-dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
+    repositories {
+        maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+        mavenCentral()
+    }
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.mockito:mockito-core:4.3.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+        testImplementation("org.mockito:mockito-core:4.3.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
+    java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    tasks.getByName<JavaCompile>("compileJava") {
+        options.apply {
+            release.set(17)
+            encoding = "UTF-8"
+        }
+    }
 
-tasks.getByName<JavaCompile>("compileJava") {
-    options.apply {
-        release.set(17)
-        encoding = "UTF-8"
+    tasks.getByName<Test>("test") {
+        useJUnitPlatform()
+    }
+
+    configure<TestLoggerExtension> {
+        theme = ThemeType.PLAIN
+        showExceptions = true
+        showStackTraces = true
+        showFullStackTraces = true
+        showCauses = true
+        slowThreshold = 2000
+        showSummary = true
+        showSimpleNames = false
+        showPassed = true
+        showSkipped = true
+        showFailed = true
+        showStandardStreams = true
+        showPassedStandardStreams = true
+        showSkippedStandardStreams = true
+        showFailedStandardStreams = true
+        logLevel = LogLevel.LIFECYCLE
     }
 }
